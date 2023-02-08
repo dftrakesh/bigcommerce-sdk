4db98f5b4f41088cfbd156678a8bcc159a68e5cd
Abhishek.Sangani <abhishek.sangani@dakshyafreondtech.com>
2023-02-08 15:17:00 +0530

    Added bigcommerce brand metafield and related methods.

--- src/main/java/com/dft/bigcommerce/model/product/brandMetafields/BrandMetafieldsWrapper.java
+++ src/main/java/com/dft/bigcommerce/model/product/brandMetafields/BrandMetafieldsWrapper.java
@@ -2,8 +2,6 @@ package com.dft.bigcommerce.model.product.brandMetafields;
 
 import com.dft.bigcommerce.model.common.Meta;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 import java.util.List;
