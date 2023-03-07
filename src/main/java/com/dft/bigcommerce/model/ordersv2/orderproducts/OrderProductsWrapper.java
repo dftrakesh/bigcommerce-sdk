d75f09ba4fdea0dfeb51e4554516f880b784466e
abhishek.sangani <abhishek.sangani@dakshyafreondtech.com>
2023-03-07 10:03:25 +0530

    Added Order Products POJOs and methods for Bigcommerce API Order V2 Products.
    Created a new orderV2Products package for version2 orders to represent orderProducts returned from the API, added POJOs, and using those POJOs, created methods to retrieve OrderProduct Objects.
    
    deleted the unnecessary package "mapper"
    
    removed unnecessary external libraries.

--- src/main/java/com/dft/bigcommerce/model/ordersv2/orderproducts/OrderProductsWrapper.java
+++ src/main/java/com/dft/bigcommerce/model/ordersv2/orderproducts/OrderProductsWrapper.java
@@ -1,9 +1,6 @@
 package com.dft.bigcommerce.model.ordersv2.orderproducts;
 
-import lombok.Data;
-
 import java.util.ArrayList;
 
-@Data
 public class OrderProductsWrapper extends ArrayList<OrderProductWrapper> {
 }
