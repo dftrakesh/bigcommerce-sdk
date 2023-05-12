db3aeffbc5a263f18e705c10d39f389ac2712daf
Hemangi.Chauhan <hemangi.chauhan@dakshyafreondtech.com>
2023-05-12 11:04:58 +0530

    RAG-195 Updated SDK for Customer Address.

--- src/main/java/com/dft/bigcommerce/BigcommerceCustomerAddresses.java
+++ src/main/java/com/dft/bigcommerce/BigcommerceCustomerAddresses.java
@@ -12,16 +12,15 @@ public class BigcommerceCustomerAddresses extends BigcommerceSDK {
 
     private static final String FORWARD_SLASH_CHARACTER = "/";
     private static final String CUSTOMERS_ENDPOINT = "/customers";
-    private static final String CUSTOMER_ADDRESSES_ENDPOINT = "/addresses";
+    private static final String CUSTOMER_ADDRESSES_ENDPOINT = "addresses";
 
     public BigcommerceCustomerAddresses(String storeHash, String accessToken) {
         super(storeHash, accessToken);
     }
 
     @SneakyThrows
-    public CustomerAddressesWrapper getAllCustomerAddress(Integer customerId) {
+    public CustomerAddressesWrapper getAllCustomerAddress() {
         URI uri = baseUrlV2(CUSTOMERS_ENDPOINT + FORWARD_SLASH_CHARACTER
-                + customerId.toString()
                 + CUSTOMER_ADDRESSES_ENDPOINT);
 
         HttpRequest request = get(uri);
