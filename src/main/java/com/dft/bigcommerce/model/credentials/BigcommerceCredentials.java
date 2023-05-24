9145d1da175f97f60ecb6f8a75378542f6e1480c
Hemangi.Chauhan <hemangi.chauhan@dakshyafreondtech.com>
2023-05-24 13:20:31 +0530

    Changed Version. Added Annotation in BigcommerceCredentials.

--- src/main/java/com/dft/bigcommerce/model/credentials/BigcommerceCredentials.java
+++ src/main/java/com/dft/bigcommerce/model/credentials/BigcommerceCredentials.java
@@ -1,10 +1,14 @@
 package com.dft.bigcommerce.model.credentials;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder(toBuilder = true)
 public class BigcommerceCredentials {
     private String storeHash;
     private String accessToken;
