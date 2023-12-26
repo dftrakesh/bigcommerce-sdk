3e9ac01413fff171a9deffbb7cb31e12c1a2f942
abhishek.sangani <abhishek.sangani@dakshyafreondtech.com>
2023-12-26 14:21:35 +0530

    JBAPP-867 Updated return type

--- src/main/java/com/dft/bigcommerce/v2/BigcommerceOrderShipmentsAPI.java
+++ src/main/java/com/dft/bigcommerce/v2/BigcommerceOrderShipmentsAPI.java
@@ -4,6 +4,7 @@ import com.dft.bigcommerce.BigcommerceSDK;
 import com.dft.bigcommerce.handler.JsonBodyHandler;
 import com.dft.bigcommerce.model.credentials.BigcommerceCredentials;
 import com.dft.bigcommerce.model.v2.ordershipments.OrderShipmentRequest;
+import com.dft.bigcommerce.model.v2.ordershipments.OrderShipmentWrapper;
 import com.dft.bigcommerce.model.v2.ordershipments.OrderShipmentsWrapper;
 
 import java.net.URI;
@@ -20,11 +21,11 @@ public class BigcommerceOrderShipmentsAPI extends BigcommerceSDK {
         super(credentials);
     }
     
-    public OrderShipmentsWrapper createOrderShipment(Integer orderId, OrderShipmentRequest orderShipmentRequest) {
+    public OrderShipmentWrapper createOrderShipment(Integer orderId, OrderShipmentRequest orderShipmentRequest) {
         URI uri = baseUrlV2(ORDERS_ENDPOINT + FORWARD_SLASH_CHARACTER + orderId + SHIPMENTS_ENDPOINT);
 
         HttpRequest request = postWithObject(uri, orderShipmentRequest);
-        HttpResponse.BodyHandler<OrderShipmentsWrapper> handler = new JsonBodyHandler<>(OrderShipmentsWrapper.class);
+        HttpResponse.BodyHandler<OrderShipmentWrapper> handler = new JsonBodyHandler<>(OrderShipmentWrapper.class);
         return getRequestWrapped(request, handler);
     }
 
